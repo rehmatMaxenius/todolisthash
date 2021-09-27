8ea38e3c846ec9f96cb109d12ea782b8dc628484
usama-maxenius <shehzadsofficial@gmail.com>
2021-09-27 16:29:28 +0500

    table on click

--- client/src/components/todos/TodoTable.jsx
+++ client/src/components/todos/TodoTable.jsx
@@ -25,7 +25,7 @@ const TodoTable = ({todos}) => {
     }, [dispatch, currentUpdateTodoId])
     return (
         <React.Fragment>
-            {todos &&
+            {todos.length &&
             <Table className="list-table mx-4" striped hover size="sm">
                 <thead>
                 <tr>
